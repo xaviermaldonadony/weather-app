eba5b8bf6cce75ce627bb0172501f3d6e478eb41
xaviermaldonadony <xaviermaldonadony@hotmail.com>
2020-09-08 01:47:15 -0400

    Set up for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -14,18 +14,16 @@ weatherForm.addEventListener('submit', (e) => {
 	messageOne.textContent = 'Loading...';
 	messageTwo.textContent = '';
 
-	fetch(`http://localhost:3000/weather?address=${location}`).then(
-		(response) => {
-			response.json().then((data) => {
-				if (data.error) {
-					messageOne.textContent = data.error;
-				} else {
-					messageOne.textContent = data.location;
-					messageTwo.textContent = data.forecast;
-					console.log(data.location);
-					console.log(data.forecast);
-				}
-			});
-		}
-	);
+	fetch(`/weather?address=${location}`).then((response) => {
+		response.json().then((data) => {
+			if (data.error) {
+				messageOne.textContent = data.error;
+			} else {
+				messageOne.textContent = data.location;
+				messageTwo.textContent = data.forecast;
+				console.log(data.location);
+				console.log(data.forecast);
+			}
+		});
+	});
 });
